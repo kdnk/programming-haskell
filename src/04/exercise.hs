01c80e975a66a294c37d31313ee1fc2017e16e31
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2023-04-18 10:50:07 +0900

    chore(wip): 04 exercise

--- src/04/exercise.hs
+++ src/04/exercise.hs
@@ -9,6 +9,16 @@ main = do
   print (third2 [1, 2, 3, 4, 5, 6])
   print (third3 [1, 2, 3, 4, 5, 6])
 
+  print (safetail1 [1, 2, 3, 4, 5, 6])
+  print (safetail2 [1, 2, 3, 4, 5, 6])
+  print (safetail3 [1, 2, 3, 4, 5, 6])
+
+  print (safetail1 [])
+  print (safetail2 [])
+  print (safetail3 [])
+  --
+  print ()
+
 halve :: [a] -> ([a], [a])
 halve xs = (take (length xs `div` 2) xs, drop (length xs `div` 2) xs)
 
@@ -21,12 +31,37 @@ third2 xs = xs !! 2
 third3 :: [a] -> a
 third3 (x : y : z : xs) = z
 
-safetail1 :: [a] -> [a]
+safetail1 :: Num a => [a] -> [a]
 safetail1 xs =
   if null xs
     then []
-    else []
+    else tail xs
 
-safetail3 :: [a] -> [a]
+safetail2 :: Num a => [a] -> [a]
+safetail2 xs
+  | null xs = []
+  | otherwise = tail xs
+
+safetail3 :: Num a => [a] -> [a]
 safetail3 [] = []
 safetail3 (x : xs) = xs
+
+-- (||) :: Bool -> Bool -> Bool
+-- True || True = True
+-- True || False = True
+-- False || True = True
+-- False || False = False
+
+(||) :: Bool -> Bool -> Bool
+False || False = False
+True || True = True
+True || False = True
+False || True = True
+
+(|||) :: Bool -> Bool -> Bool
+False ||| False = False
+_ ||| _ = True
+
+(||||) :: Bool -> Bool -> Bool
+True |||| _ = True
+False |||| b = b
