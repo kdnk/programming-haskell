3b32137ae1fbfa1ea36ba5c848e2e5c1584e728c
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2023-05-02 17:26:07 +0900

    caesar

--- src/05/caesar.hs
+++ src/05/caesar.hs
@@ -13,6 +13,16 @@ main = do
   print (encode 3 "haskell is fun")
   print (encode (-3) "kdvnhoo lv ixq")
 
+  print (percent 5 15)
+
+  print (freqs "abbcccddddeeeee")
+
+  print (rotate 3 [1, 2, 3, 4, 5])
+
+  print table
+
+  print (crack "kdvnhoo lv ixq")
+
   print ()
 
 -- 小文字のみを暗号化
@@ -31,3 +41,38 @@ shift n c
 
 encode :: Int -> String -> String
 encode n xs = [shift n x | x <- xs]
+
+lowers :: String -> Int
+lowers xs = length [x | x <- xs, 'a' <= x && x <= 'z']
+
+count :: Char -> String -> Int
+count x xs = length [x | x' <- xs, x == x']
+
+percent :: Int -> Int -> Float
+percent n m = (fromIntegral n / fromIntegral m) * 100
+
+-- 任意の文字に対して、出現頻度表を返す関数
+freqs :: String -> [Float]
+freqs xs = [percent (count x xs) (lowers xs) | x <- ['a' .. 'z']]
+
+-- カイ二乗検定
+chisqr :: [Float] -> [Float] -> Float
+chisqr os es = sum [((o - e) ^ 2) / e | (o, e) <- zip os es]
+
+rotate :: Int -> [a] -> [a]
+rotate n xs = after ++ before
+  where
+    (before, after) = splitAt n xs
+
+table :: [Float]
+table = [8.1, 1.5, 2.8, 4.2, 12.7, 2.2, 2.0, 6.1, 7.0, 0.2, 0.8, 4.0, 2.4, 6.7, 7.5, 1.9, 0.1, 6.0, 6.3, 9.0, 2.8, 1.0, 2.4, 0.2, 2.0, 0.1]
+
+positions :: Eq a => a -> [a] -> [Int]
+positions x xs = [i | (x', i) <- zip xs [0 ..], x' == x]
+
+crack :: String -> String
+crack xs = encode (-factor) xs
+  where
+    table' = freqs xs
+    chitable = [chisqr (rotate n table') table | n <- [0 .. 25]]
+    factor = head (positions (minimum chitable) chitable)
