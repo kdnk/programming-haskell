77ea4861bebbfb09c0e190fcffd16685a201b7c1
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2023-07-03 23:44:08 +0900

    wip: 08

--- src/08/main.hs
+++ src/08/main.hs
@@ -28,3 +28,36 @@ safediv a b = Just (a `div` b)
 safehead :: [a] -> Maybe a
 safehead [] = Nothing
 safehead xs = Just (head xs)
+
+data Nat = Zero | Succ Nat
+
+nat2int :: Nat -> Int
+nat2int Zero = 0
+nat2int (Succ n) = 1 + nat2int n
+
+int2nat :: Int -> Nat
+int2nat 0 = Zero
+int2nat n = Succ (int2nat (n - 1))
+
+add :: Nat -> Nat -> Nat
+add m n = int2nat (nat2int m + nat2int n)
+
+add2 :: Nat -> Nat -> Nat
+add2 Zero n = n
+add2 (Succ m) n = Succ (add m n)
+
+data List a = Nil | Cons a (List a)
+
+len :: List a -> Int
+len Nil = 0
+len (Cons _ xs) = 1 + len xs
+
+data Tree a = Leaf a | Node (Tree a) a (Tree a)
+
+occurs :: Eq a => a -> Tree a -> Bool
+occurs x (Leaf y) = x == y
+occurs x (Node l y r) = x == y || occurs x l || occurs x r
+
+flatten :: Tree a -> [a]
+flatten (Leaf x) = [x]
+flatten (Node l y r) = flatten l ++ [y] ++ flatten r
